e20abd2459a661a833a777bd6e0e3534912a7b64
Leon Timmermans <fawaka@gmail.com>
2013-08-30 13:41:02 +0200

    Convert to dzil

--- lib/Thread/Channel.pm
+++ lib/Thread/Channel.pm
@@ -3,85 +3,22 @@ package Thread::Channel;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+use Storable 2.05 ();
+
+use XSLoader;
+XSLoader::load('Thread::Channel', __PACKAGE__->VERSION);
 
 1; # End of Thread::Channel
 
-__END__
+# ABSTRACT: Fast thread queues
 
-=head1 NAME
+=head1 DESCRIPTION
 
-Thread::Channel - The great new Thread::Channel!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
 
-Perhaps a little code snippet.
-
-    use Thread::Channel;
-
-    my $foo = Thread::Channel->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=head1 AUTHOR
-
-Leon Timmermans, C<< <leont at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-thread-channel at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Thread-Channel>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Thread::Channel
+=method
 
 
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Thread-Channel>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Thread-Channel>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Thread-Channel>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Thread-Channel>
-
-=back
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Leon Timmermans, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Thread::Channel' );
-}
-
-diag( "Testing Thread::Channel $Thread::Channel::VERSION, Perl $], $^X" );
