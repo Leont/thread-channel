a3a12d360290a8db2ebf11a93cedab15e7ccb769
Leon Timmermans <fawaka@gmail.com>
2013-08-30 13:42:34 +0200

    Initial implementation

---
+++ src/queue.h
@@ -0,0 +1,19 @@
+typedef message queue_node;
+
+typedef struct {
+	perl_mutex mutex;
+	perl_cond condvar;
+	message* front;
+	message* back;
+	IV refcount;
+} message_queue;
+
+void queue_init(message_queue*);
+void S_queue_enqueue(pTHX_ message_queue* queue, const message* message, perl_mutex* lock);
+#define queue_enqueue(queue, message, lock) S_queue_enqueue(aTHX_ queue, message, lock)
+const message* S_queue_dequeue(pTHX_ message_queue* queue, perl_mutex* lock);
+#define queue_dequeue(queue, lock) S_queue_dequeue(aTHX_ queue, lock)
+const message* S_queue_dequeue_nb(pTHX_ message_queue* queue, perl_mutex* lock);
+#define queue_dequeue_nb(queue, lock) S_queue_dequeue_nb(aTHX_ queue, lock)
+void S_queue_destroy(pTHX_ message_queue*);
+#define queue_destroy(queue) S_queue_destroy(aTHX_ queue)
